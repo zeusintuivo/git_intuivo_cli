20e36b64188f25f60aed823cdb4973391e1806ba
zeusintuivo <zeus@intuivo.com>
2021-03-14 14:09:55 +0100

    added pull scripts with subdir new function

---
+++ pullsubs2.sh
@@ -0,0 +1,55 @@
+.dirs() {
+    find * -maxdepth 0 -type d   # mac and linux tested
+}
+# DEBUG=1
+# REMOVECACHE=1
+.subdir() {
+  # Perform all actions in
+  #        LIST1
+  #          for each element in
+  #                           LIST2
+
+  local _curdir="${1}"
+  local ACTIONS="${2}"
+  local local_items="$(.dirs)"
+  local one_item
+  local one_action
+  local action
+  local _cwd="$(pwd)"
+
+  while read -r one_item; do
+  {
+    if [ ! -z "${one_item}" ] ; then  # if not empty
+    {
+      action="${one_action/\{\#\}/$one_item}"  # replace value inside string substitution expresion bash
+      eval ${action}
+    }
+    fi
+  }
+  done <<< "${local_items}"
+}
+
+
+CURDIR="${PWD}"
+
+ACTIONS="echo -e \".\"
+echo \" \"
+echo \"Directory:\"
+echo \". \"
+echo \". . . . . . .       {#} \"
+cd \"${CURDIR}/{#}\"
+pwd
+[[ -d .git/ ]] && pull
+"
+ACTIONS="
+      cd \"${_cwd}/${#}\"
+      pwd
+      [[ -d .git/ ]] && pull
+      if [[ ! -d .git/ ]] ; then
+      {
+        .subdir \"${_cwd}/${#}\" "${ACTIONS}"
+      }
+      fi
+"
+.subdir  "${CURDIR}" "${ACTIONS}" 
+
